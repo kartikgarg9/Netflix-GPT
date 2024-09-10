146e9717abf93144d0f27ef21716d622d0cc91ab
kartikgarg9 <KARTIKGARG20@GMAIL.COM>
2024-09-10 12:38:47 +0530

    TMDB API data Fetch && Built Main Container

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR =
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkM2JlNzQzMTI5NTg2YzdjN2M3NDQ3YWVjMTAxYzgzYiIsIm5iZiI6MTcyNTg3MDY0MS45NDMxMDgsInN1YiI6IjY2ZGViMDUyMDRmNjJhMGU2NDQ5Yjg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HFKIo_AwPQ0fGBJ3SQxwP8K83dNgJQkVK8D3qWOUP9o",
+  },
+};
